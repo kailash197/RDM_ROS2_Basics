8f4031e7196bc2e5131e14de69f18646ef8debc4
Kailash Khadka <k.khadka343@gmail.com>
2024-10-13 02:29:32 +0000

    Topic publishers, composable

---
+++ topic_publisher_pkg/launch/simple_topic_publisher.launch.py
@@ -0,0 +1,14 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='topic_publisher_pkg',
+            executable='simple_publisher_node',
+            output='screen'), 
+        Node(
+            package='topic_publisher_pkg',
+            executable='simple_publisher_composable_node',
+            output='screen'),
+    ])
